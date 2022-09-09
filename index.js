6cdd60122da2b443a0185801bec5137223a6385b
Uzziel <uzzielkk@gmail.com>
2022-09-09 12:12:33 +0300

    Changed operation port back to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 3005
+const port = 3000
 
 app.get('/covid', (req, res) => {
   //Get the Kenyan data from WHO
